b1920a7df45e69866f4080ae36987eb7e2342df6
lopezatn <agustinlpzb@gmail.com>
2025-12-08 12:27:38 +0100

    added the ASG terraform cfg resource

---
+++ phase3-IaC-automation/terraform-sysdev-ec2/autoscaling.tf
@@ -0,0 +1,24 @@
+resource "aws_autoscaling_group" "sysdev_asg" {
+  name                 = "sysdev-asg"
+  max_size             = 2
+  min_size             = 1
+  desired_capacity     = 1
+
+  vpc_zone_identifier = [
+    "subnet-0a3ab44f6771e1f6d"
+  ]
+
+  launch_template {
+    id      = aws_launch_template.sysdev_web_lt.id
+    version = "$Latest"
+  }
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  tag {
+    key                 = "Name"
+    value               = "sysdev-web-server"
+    propagate_at_launch = true
+  }
+}
