d3e831d7e207a29bd622f750178b83dda8c90eaa
lopezatn <agustinlpzb@gmail.com>
2025-12-18 14:59:23 +0100

    Added asg-scaling.tf and alarms.tf to handle TargetResponseTime

---
+++ phase3-IaC-automation/terraform-sysdev-ec2/alarms.tf
@@ -0,0 +1,17 @@
+resource "aws_cloudwatch_metric_alarm" "asg_cpu_high" {
+  alarm_name          = "sysdev-asg-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 5
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+
+  namespace   = "AWS/EC2"
+  metric_name = "CPUUtilization"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.sysdev_asg.name
+  }
+
+  alarm_actions = [aws_sns_topic.sysdev_alerts.arn]
+}
