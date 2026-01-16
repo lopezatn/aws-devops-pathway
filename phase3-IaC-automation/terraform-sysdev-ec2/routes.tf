a2c381db49176e81ddaaaed95b420167af2cb60f
lopezatn <agustinlpzb@gmail.com>
2026-01-16 18:41:03 +0100

    public and private route tables added

---
+++ phase3-IaC-automation/terraform-sysdev-ec2/routes.tf
@@ -0,0 +1,45 @@
+resource "aws_route_tables" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "webhost-prod-public-rt"
+    Environment = "prod"
+    System      = "webhost"
+    Tier        = "public"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_tables.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_tables.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "webhost-prod-private-rt"
+    Environment = "prod"
+    System      = "webhost"
+    Tier        = "private"
+  }
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id  
+}
